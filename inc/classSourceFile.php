e9ca6ba796a8e8c8c856f4325dc5eddb0a348496
Jeff Soo <jeff@ipsedixit.net>
2011-01-24 12:55:37 -0500

    Added LICENSE and README
    Whitespace pruning

--- inc/classSourceFile.php
+++ inc/classSourceFile.php
@@ -1,116 +1,116 @@
 <?php
 	
-	class SourceFile
-	{
-		protected $page_title;
-		protected $sort_order;
+class SourceFile
+{
+	protected $page_title;
+	protected $sort_order;
 
-		private $_name;
-		private $_source;
-		private $_html;
-		private $_parser;
-		private $_lang;
-		private $_untranslated = false;
-		
-		public function __construct($name, $file_path, $parser, $lang)
+	private $_name;
+	private $_source;
+	private $_html;
+	private $_parser;
+	private $_lang;
+	private $_untranslated = false;
+	
+	public function __construct($name, $file_path, $parser, $lang)
+	{
+		$this->_name = $name;
+		$this->_parser = $parser;
+		$this->_lang = $lang;
+		if ( file_exists($file_path) )
 		{
-			$this->_name = $name;
-			$this->_parser = $parser;
-			$this->_lang = $lang;
-			if ( file_exists($file_path) )
+			$this->_source = file_get_contents($file_path);
+			$lines = explode("\n", $this->_source);
+			foreach ( $lines as $line )
 			{
-				$this->_source = file_get_contents($file_path);
-				$lines = explode("\n", $this->_source);
-				foreach ( $lines as $line )
+				if ( preg_match('/^\s*$/', $line) )
+					break;
+				foreach ( $this as $meta => $value )
 				{
-					if ( preg_match('/^\s*$/', $line) )
-						break;
-					foreach ( $this as $meta => $value )
+					if ( substr($line, 0, strlen($meta)) === $meta )
 					{
-						if ( substr($line, 0, strlen($meta)) === $meta )
-						{
-							$this->$meta = substr($line, strlen($meta) + strlen(' => '));
-						}
+						$this->$meta = substr($line, strlen($meta) + strlen(' => '));
 					}
 				}
 			}
 		}
-		
-		public function __get($property)
-		{
-			$getter = 'get_' . $property;
-			if ( method_exists($this, $getter) )
-				return $this->$getter();
-		}
-		
-		public function get_page_title()
-		{
-			return $this->page_title;
-		}
-		
-		public function get_sort_order()
-		{
-			return $this->sort_order;
-		}
-		
-		public function get_name()
-		{
-			return $this->_name;
-		}
-		
-		public function get_html()
-		{
-			return htmlspecialchars($this->_get_html());
-		}
-		
-		public function get_web()
-		{
-			return $this->_get_html();
-		}
-		
-		public function get_source()
-		{
-			return $this->_source;
-		}
-		
-		private function _get_html()
-		{
-			if ( ! $this->_html )
-			{
-				$this->_html = $this->_parse();
-			}
-			return $this->_html;
-		}
-		
-		public function set_lang($lang)
-		{
-			$this->_lang = $lang;
-			return $this;
-		}
-		
-		public function get_lang()
-		{
-			return $this->_lang;
-		}
-		
-		public function set_untranslated($bool)
-		{
-			$this->_untranslated = $bool;
-			return $this;
-		}
-		
-		public function is_untranslated()
-		{
-			return $this->_untranslated;
-		}
-		
-		private function _parse($source = '')
-		{
-			if ( ! $source )
-				$source = $this->_source;
-			if ( get_class($this->_parser) === 'Textile' )
-				return $this->_parser->TextileThis($source);
-		}
-		
 	}
+	
+	public function __get($property)
+	{
+		$getter = 'get_' . $property;
+		if ( method_exists($this, $getter) )
+			return $this->$getter();
+	}
+	
+	public function get_page_title()
+	{
+		return $this->page_title;
+	}
+	
+	public function get_sort_order()
+	{
+		return $this->sort_order;
+	}
+	
+	public function get_name()
+	{
+		return $this->_name;
+	}
+	
+	public function get_html()
+	{
+		return htmlspecialchars($this->_get_html());
+	}
+	
+	public function get_web()
+	{
+		return $this->_get_html();
+	}
+	
+	public function get_source()
+	{
+		return $this->_source;
+	}
+	
+	private function _get_html()
+	{
+		if ( ! $this->_html )
+		{
+			$this->_html = $this->_parse();
+		}
+		return $this->_html;
+	}
+	
+	public function set_lang($lang)
+	{
+		$this->_lang = $lang;
+		return $this;
+	}
+	
+	public function get_lang()
+	{
+		return $this->_lang;
+	}
+	
+	public function set_untranslated($bool)
+	{
+		$this->_untranslated = $bool;
+		return $this;
+	}
+	
+	public function is_untranslated()
+	{
+		return $this->_untranslated;
+	}
+	
+	private function _parse($source = '')
+	{
+		if ( ! $source )
+			$source = $this->_source;
+		if ( get_class($this->_parser) === 'Textile' )
+			return $this->_parser->TextileThis($source);
+	}
+	
+}
 
